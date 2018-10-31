5245d1fd0aa368dd988ae2b6e97e49207ed83efc
bencat <benoit.catillon.dev@gmail.com>
2018-10-31 14:16:37 +0100

    Get all posts and display them + count of 'love'

--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post-list-item',
@@ -7,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListItemComponent implements OnInit {
 
+  @Input() postTitle: string;
+  @Input() postContent: string;
+  @Input() postCreatedAt: string;
+  @Input() postLoveIts: number;
+
   constructor() { }
+
+  onLoveIts() {
+      if (this.postLoveIts >= 0) {
+          this.postLoveIts++;
+          console.log(this.postLoveIts);
+      }
+
+  }
+
+  onNotLoveIts() {
+      if (this.postLoveIts > 0) {
+          this.postLoveIts--;
+          console.log(this.postLoveIts);
+      }
+  }
 
   ngOnInit() {
   }
